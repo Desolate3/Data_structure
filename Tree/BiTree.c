22c733d799ea4932574dcb32ad54e23ea407ae30
Desolate3 <1925495246@qq.com>
2025-07-29 22:42:15 +0800

    增加二叉树-树的建立和遍历

--- Tree/BiTree.c
+++ Tree/BiTree.c
@@ -8,28 +8,34 @@
 typedef struct{
     int value;
 } ElemType1;
-typedef struct{
+typedef struct BiTNode{
     ElemType1 data;
     struct BiTNode *left;
     struct BiTNode *right;
 } BiTNode;
 
+BiTNode *final = NULL;
+BiTNode *pre = NULL;
 
 
-//访问结点
+//打印结点
 void visit(BiTNode *node) {
     if (node != NULL) {
-        printf("%d ", node->data.value);
+        printf("%d\n", node->data.value);
     }
 }
 
 //先序遍历
-void PreOrder(BiTNode *T){
-    if(T == NULL)
+void PreOrder(BiTNode *p, BiTNode *q){
+    if(p == NULL)
         return;
-    visit(T);
-    PreOrder(T->left);
-    PreOrder(T->right);
+    if(q == p){
+        final = pre;
+    }
+    else
+        pre = p;
+    PreOrder(p->left, q);
+    PreOrder(p->right, q);
 }
 
 
@@ -40,12 +46,32 @@ int main(){
     root->left = NULL;
     root->right = NULL;
 
-    //创建左子结点
+    //创建子结点
     BiTNode *leftChild = (BiTNode *)malloc(sizeof(BiTNode));
     leftChild->data.value = 2;
     leftChild->left = NULL;
     leftChild->right = NULL;
     root->left = leftChild;
 
+    BiTNode *rightChild = (BiTNode *)malloc(sizeof(BiTNode));
+    rightChild->data.value = 3;
+    rightChild->left = NULL;
+    rightChild->right = NULL;
+    root->right = rightChild;
+
+    BiTNode *leftChild1 = (BiTNode *)malloc(sizeof(BiTNode));
+    leftChild1->data.value = 4;
+    leftChild1->left = NULL;
+    leftChild1->right = NULL;
+    leftChild->left = leftChild1;
+
+    BiTNode *q = (BiTNode *)malloc(sizeof(BiTNode));
+    q->data.value = 5;
+    q->left = NULL;
+    q->right = NULL;
+    rightChild->left = q;
+    
+    PreOrder(root, rightChild);
+    visit(final);
     return 0;
 }
